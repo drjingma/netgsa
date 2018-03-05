a9fff138c9660e44f701e01e7fc2355ae97eb953
Jing <jingma@PHS-Jing-Ma-MacBook-Pro-B02336.local>
2018-03-04 21:08:37 -0800

    update data.R

--- R/data.R
+++ R/data.R
@@ -1,13 +1,13 @@
 #' Data matrix p by n 
 'x' 
-    #' A 0-1 adjacency matrix corresponding to the known edges
-    'A' 
-    #' The vector of class indicators 
-    'group'
-    #' A list of KEGG pathways
-    'pathways'
-    #' A data frame of edges, each row corresponding to one edge
-    'edgelist'
-    #' A data frame of nonedges, each row corresponding to one negative edge
-    'nonedgelist'
+#' A directed acyclic graph corresponding to the Adrenergic signaling in cardiomyocytes pathway from KEGG
+'g' 
+#' The vector of class indicators 
+'group'
+#' A list of KEGG pathways
+'pathways'
+#' A data frame of edges, each row corresponding to one edge
+'edgelist'
+#' A data frame of nonedges, each row corresponding to one negative edge
+'nonedgelist'
     
