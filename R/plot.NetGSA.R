6bb4cc295a42003f6406a1845ae8290c46be4583
Mike Hellstern <mike.hellstern2@gmail.com>
2019-08-21 14:52:51 -0700

    Added plotting functions

---
+++ R/plot.NetGSA.R
@@ -0,0 +1,238 @@
+plot.NetGSA <- function(x, graph_layout = NULL, rescale_node = c(2,10), rescale_label = c(0.5,0.6)){
+  edges_pathways_list <- makePathwayEdges(x$graph$edgelist, x$graph$pathways)
+  edges_pathways      <- edges_pathways_list[["edges_pathways"]]
+  edges_all           <- edges_pathways_list[["edges_all"]]
+  
+  cytoscape_open      <- tryCatch({httr::GET("http://localhost:1234/v1/version")$status_code == 200}, error = function(e){return(FALSE)})
+  
+  if(cytoscape_open){
+    cyto <- plot_cytoscape.NetGSA(edges_pathways, edges_all, x$graph$pathways, x$results, x$graph$gene.tests, graph_layout = graph_layout)
+    legend_title <- "Cytoscape plot legend"
+  } else{
+    cyto <- NULL
+    legend_title <- "Igraph plot legend"
+  }
+  plotCytoLegend(x$results, legend_title, igraph = ! cytoscape_open)
+  plot_igraph.NetGSA(edges_pathways, x$results, x$graph$pathways, nodes_layout = cyto[["node_locations"]], cytoscape_open = cytoscape_open, graph_layout = graph_layout, rescale_node = rescale_node, rescale_label = rescale_label)
+  #Reset plot layout
+  par(mfrow=c(1,1))
+}
+
+
+plot_cytoscape.NetGSA <- function(edges_pathways, edges_all, pathway_gene_map, pathway_results, gene_results, graph_layout = NULL, title = "Pathway Network"){
+  title <-  gsub(" ", "\\ ", title, fixed = TRUE)
+  #Using Cytoscape
+  network_ids         <- createNestedNetwork(edges_pathways = edges_pathways, edges_all = edges_all, pathway_vertices = pathway_gene_map, main = title)
+  
+  #Now we add data to the nodes. Could do this all through cyrest, but thankfully RCy3 has a helper fn for us
+  #Also adding edge weights incase we want that
+  RCy3::setCurrentNetwork(network_ids$networks[1])
+  edge_df             <- setnames(copy(edges_pathways)[, id := paste0(src_pathway, " (pp) ", dest_pathway)], "weight_sum", "weight")
+  RCy3::loadTableData(data = pathway_results, data.key.column = "pathway", table = "node", table.key.column = "name", network = "current")
+  RCy3::loadTableData(data = edge_df, data.key.column = "id", table = "edge", table.key.column = "name", network = "current")
+  if(is.null(graph_layout)){
+    graph_layout = 'force-directed defaultSpringCoefficient=0.00000004 defaultSpringLength=100'
+  }
+  RCy3::layoutNetwork(graph_layout) ##Ooohhh this one is good!
+  node_locations      <- getCytoscapeXYCoords(network_ids$networks[1])
+  
+  #Adding mappings
+  teststat_lim <- max(abs(min(pathway_results$teststat)), abs(max(pathway_results$teststat)))
+  #Mappings are constant for a given style. So these are really editing the "default" visual style mapping. This is done for ALL networks that use "default". Not just the network specified
+  RCy3::copyVisualStyle("default", "pathway_style")
+  RCy3::copyVisualStyle("default", "gene_style") #For use later
+  
+  #Setting up "pathway_style"
+  RCy3::setNodeColorMapping("teststat", c(-teststat_lim, 0, teststat_lim), c("#FFA500", "#FFFFFF", "#0000FF"), mapping.type = "continuous", style.name = "pathway_style")
+  RCy3::setNodeBorderColorMapping("pFdr", c(0,1), c("#FF0000", "#FFFFFF"), mapping.type = "continuous", style.name = "pathway_style")
+  RCy3::setNodeWidthMapping("pSize", c(min(pathway_results$pSize), max(pathway_results$pSize)), c(20, 120), style.name = "pathway_style")
+  RCy3::setNodeHeightMapping("pSize", c(min(pathway_results$pSize), max(pathway_results$pSize)), c(20, 120), style.name = "pathway_style")
+  RCy3::setNodeTooltipMapping("pathway", style.name = "pathway_style")
+  
+  #These are for specific nodes. Only for network specified
+  RCy3::setNodeBorderWidthBypass(pathway_results$pathway, 10, network = network_ids$networks[1])
+  RCy3::setNodeShapeBypass(pathway_results$pathway, "ellipse", network = network_ids$networks[1]) #I was getting weird error because some of the nodes werent added!
+  RCy3::setVisualStyle("pathway_style")
+  
+  return(list(node_locations = node_locations))
+}
+
+
+plot_igraph.NetGSA <- function(edges_pathways, pathway_results, pathway_gene_map, nodes_layout = NULL, cytoscape_open = FALSE, graph_layout = NULL, rescale_node = c(2,10), rescale_label = c(0.5,0.6)){
+  if(is.null(nodes_layout)) nodes_list <- pathway_results[,"pathway", drop = FALSE]
+  else                      nodes_list <- nodes_layout
+  #Using igraph if Cytoscape not available
+  ig                      <- igraph::graph_from_data_frame(edges_pathways, directed = FALSE, vertices = nodes_list)
+  pathway_results_ordered <- pathway_results[match(igraph::V(ig)$name, pathway_results$pathway), ] #Make sure this is in correct order
+  
+  ## Color vertex outline based on FDR
+  fdr_cols                <- c("red", "white") #Red is small vals (sig) white is large vals (insig)
+  fdr_ramp                <- colorRamp(fdr_cols) #Coloring p1
+  fdr_rgb_vals            <- rgb(fdr_ramp(pathway_results_ordered$pFdr)/255) #Coloring p2
+  
+  ## Color vertex based on teststat
+  teststat_lim            <- max(abs(min(pathway_results_ordered$teststat)), abs(max(pathway_results_ordered$teststat)))
+  teststat_cols           <- c("orange", "white", "blue") #Orange is small vals (-), blue is large vals (+)
+  teststat_ramp           <- colorRamp(teststat_cols)
+  std_teststat            <- (pathway_results_ordered$teststat - (-teststat_lim)) / (teststat_lim - (-teststat_lim))
+  teststat_rgb_cols       <- rgb(teststat_ramp(std_teststat) /255)
+  
+  #Vertex size - scale to between 5 and 40
+  pSize_0_1_rescale       <- (pathway_results_ordered$pSize - (min(pathway_results_ordered$pSize))) / (max(pathway_results_ordered$pSize) - min(pathway_results_ordered$pSize))
+  vertex_sizes            <- pSize_0_1_rescale*(rescale_node[2] - rescale_node[1]) + rescale_node[1]
+  
+  label_sizes             <- pSize_0_1_rescale*(rescale_label[2] - rescale_label[1]) + rescale_label[1]
+  
+  if (cytoscape_open)              l <- NULL #Change to layout of cytoscape
+  else if (!is.null(graph_layout)) l <- graph_layout(ig)
+  else                             l <- igraph::layout_on_sphere(ig)
+  
+  #Cytoscape open, use coords and plot ig as well as legend on side
+  if(cytoscape_open){
+    layout_mat <- matrix(c(1,1,2,3), nrow = 2)
+    layout(layout_mat, widths = c(8,2))
+    par(mai=c(0.5, 0.5, 0.5, 0.5))
+    plot(ig, vertex.color = teststat_rgb_cols, vertex.frame.color = fdr_rgb_vals, vertex.size = vertex_sizes, vertex.label.cex = 0.5, layout = l) 
+    
+    teststat_image <- as.raster(matrix(rev(colorRampPalette(teststat_cols)(20)), ncol=1))
+    plot(c(0,0.5),c(0,1),type = 'n', axes = F,xlab = '', ylab = '')
+    text(x=0.225, y = c(0,0.5,1), labels = round(c(-teststat_lim, 0, teststat_lim), digits = 1))
+    rasterImage(teststat_image, 0, 0, 0.15,1)
+    mtext("Node Color\nTest statistic", side = 3, at = c(0.075))
+    
+    fdr_image <- as.raster(matrix(rev(colorRampPalette(fdr_cols)(20)), ncol=1))
+    plot(c(0,0.5),c(0,1),type = 'n', axes = F,xlab = '', ylab = '')
+    text(x=0.225, y = c(0,1), labels = c(0,1))
+    rasterImage(fdr_image, 0, 0, 0.15,1)
+    mtext("Node Border\nFDR adjusted p-value", side = 3, at = c(0.075))
+    
+  } else{
+    #plotCytoLegend(pathway_results, title = "Igraph color legend", igraph = TRUE) #Plot legend in base R because looks better
+    igraph::rglplot(ig, vertex.color = teststat_rgb_cols, vertex.size = vertex_sizes, vertex.label.cex = label_sizes, edge.color = "grey98", label.dist = 0, layout = l) #Label size not supported in RGL. No other options to make labels more visible. Label.degree doesnt put them on axis like I thought
+  }
+}
+
+plotCytoLegend <- function(pathway_results, title = "Cytoscape plot legend", igraph = FALSE){
+  fdr_cols                <- c("red", "white") #Red is small vals (sig) white is large vals (insig)
+  teststat_cols           <- c("orange", "white", "blue") #Orange is small vals (-), blue is large vals (+)
+  teststat_lim            <- max(abs(min(pathway_results$teststat)), abs(max(pathway_results$teststat)))
+  
+  
+  par(mar=c(2.5,2.5,1,1))
+  if (igraph){
+    layout_mat <- matrix(c(1,2), nrow = 2)
+    layout(layout_mat, heights = c(2,7))
+  } else{
+    layout_mat <- matrix(c(1,2,3), nrow = 3)
+    layout(layout_mat, heights = c(1,3,3))
+  }
+  
+  
+  #plot(c(0,1),c(0,0.5), type = "n", axes = F, xlab = "", ylab = "");text(x = 0.5, y = 0, "Cytoscape color legend")
+  plot.new()
+  text(0.5,0.5,title,cex=2,font=2)
+  
+  teststat_image <- as.raster(matrix(colorRampPalette(teststat_cols)(20), nrow=1))
+  plot(c(0,1),c(0,0.5),type = 'n', axes = F,xlab = '', ylab = '')
+  text(y=0.175, x = c(0,0.5,1), labels = round(c(-teststat_lim, 0, teststat_lim), digits = 2))
+  rasterImage(teststat_image, 0, 0.25, 1,0.5)
+  mtext("Node Color\nTest statistic", side = 3, at = c(0.5))
+  
+  if (!igraph){
+    fdr_image <- as.raster(matrix(colorRampPalette(fdr_cols)(20), nrow =1))
+    plot(c(0,1),c(0,0.5),type = 'n', axes = F,xlab = '', ylab = '')
+    text(y=0.175, x = c(0,1), labels = round(c(0,1), digits = 2))
+    rasterImage(fdr_image, 0, 0.25, 1,0.5)
+    mtext("Node Border\nFDR adjusted p-value (q-value)", side = 3, at = c(0.5)) 
+  }
+}
+
+#Returns cytoscape X & Y coordinates for given network
+getCytoscapeXYCoords <- function(network){
+  RCy3::setCurrentNetwork(network)
+  nodes_all <- RCy3::getAllNodes()
+  #For some reason igraph flips the y. So we need to do -y from cytoscape to get same plot in igraph
+  return(data.table(nodes = nodes_all, x = RCy3::getNodeProperty(nodes_all, "NODE_X_LOCATION"), y = -RCy3::getNodeProperty(nodes_all, "NODE_Y_LOCATION")))
+}
+
+
+
+
+formatPathways <- function(x, pways, graph_layout = NULL){
+  gene_results <- x$graph$gene.tests
+  #Layout of each pathway - Slow & prone to crashing
+  for (pway in pways){
+    RCy3::setCurrentNetwork(pway)
+    net <- RCy3::getNetworkSuid()
+    curr_nodes <- unlist(RCy3::getAllNodes())
+    #Set and edit our new visual style
+    RCy3::loadTableData(data = gene_results[J(curr_nodes), ], data.key.column = "gene", table = "node", table.key.column = "name", network = net)
+    #Setting up "gene_style"
+    RCy3::setNodeColorMapping("pFdr", c(0,1), c("#FF0000", "#FFFFFF"), mapping.type = "continuous", style.name = "gene_style")
+    RCy3::setNodeTooltipMapping("gene", style.name = "gene_style")
+    RCy3::setNodeShapeDefault("ellipse", style.name = "gene_style") #I was getting weird error because some of the nodes werent added!
+    #setNodeBorderWidthDefault(10, style.name = "gene_style")
+    RCy3::setNodeWidthDefault(75, style.name = "gene_style")
+    RCy3::setNodeHeightDefault(75, style.name = "gene_style")
+    RCy3::setVisualStyle("gene_style", network = net)
+    if(is.null(graph_layout)){
+      layout_str = 'force-directed defaultSpringCoefficient=0.00000004 defaultSpringLength=100'
+    }
+    RCy3::layoutNetwork(layout_str)
+  }
+}
+
+
+
+zoomPathway <- function(x, pway, graph_layout = NULL){
+  if (!pway %in% x$graph$pathways$pathway) stop(paste0("Pathway: \"", pway, "\" not found in list of pathways"))
+  edges_pathways_list <- makePathwayEdges(x$graph$edgelist, x$graph$pathways)
+  edges_all           <- edges_pathways_list[["edges_all"]]
+  pathway_gene_edges  <- edges_all[src_pathway ==  pway & dest_pathway == pway]
+  pathway_graph       <- igraph::graph_from_data_frame(pathway_gene_edges[, .(base_gene_src, base_gene_dest)], directed = FALSE, vertices = unique(x$graph$pathways[pathway == pway,][["gene"]]))
+  
+  if(!is.null(graph_layout)) l <- graph_layout(pathway_graph)
+  else                       l <- igraph::layout_with_graphopt(pathway_graph, spring.length = 300, spring.constant = 0.00000004)
+  plot(pathway_graph, main = pway, layout = l)
+}
+
+
+
+# Helper functions -----------------------------------------------------------------
+#Return just the edges between pathways & the edges with pathways merged on (for the nested network)
+#Delete self edges
+makePathwayEdges <- function(gene_edges, pathway_gene_map){
+  res <- gene_edges[pathway_gene_map, .(frequency, base_gene_src, base_gene_dest, src_pathway = i.pathway), on = .(base_gene_src = gene), allow.cartesian = TRUE, nomatch = 0L][
+    pathway_gene_map, .(frequency, base_gene_src, base_gene_dest, src_pathway, dest_pathway = i.pathway), on = .(base_gene_dest = gene), allow.cartesian = TRUE, nomatch = 0L]
+  return(list(edges_all = res, edges_pathways = res[src_pathway != dest_pathway, .(weight_sum = sum(frequency)), by = .(src_pathway, dest_pathway)]))
+}
+
+createNestedNetwork <- function(edges_pathways, edges_all, pathway_vertices, main){
+  
+  #Copy so doesn't update outside of function
+  edges_pathways   <- copy(edges_pathways)[, c("src_pathway", "dest_pathway") := lapply(.SD, function(x) gsub(" ", "\\ ", x, fixed = TRUE)), .SDcols = c("src_pathway", "dest_pathway")]
+  edges_all        <- copy(edges_all)[, c("base_gene_src", "base_gene_dest", "src_pathway", "dest_pathway") := lapply(.SD, function(x) gsub(" ", "\\ ", x, fixed = TRUE)), .SDcols = c("base_gene_src", "base_gene_dest", "src_pathway", "dest_pathway")]
+  pathway_vertices <- copy(pathway_vertices)[, c("pathway", "gene") := lapply(.SD, function(x) gsub(" ", "\\ ", x, fixed = TRUE))]
+  
+  pathways_edges_nnf     <- paste0(main, " ", edges_pathways$src_pathway, " pp ", edges_pathways$dest_pathway)
+  no_edge_vertices_nnf   <- paste0(main, " ", setdiff(unique(pathway_vertices$pathway), unique(c(edges_pathways$src_pathway, edges_pathways$dest_pathway))))
+  
+  
+  #Ones we want to link
+  pathways_gene_vertices <- paste0(pathway_vertices$pathway, " ", pathway_vertices$gene)
+  gene_edges_nnf_in      <- edges_all[src_pathway==dest_pathway]
+  gene_edges_nnf_in2     <- paste0(gene_edges_nnf_in$src_pathway, " ", gene_edges_nnf_in$base_gene_src, " pp ", gene_edges_nnf_in$base_gene_dest)
+  
+  #Making NNF File
+  all_nnf  <- c(pathways_edges_nnf, no_edge_vertices_nnf, pathways_gene_vertices, gene_edges_nnf_in2)
+  temp_loc <- paste0(tempfile(),".nnf")
+  f        <- base::file(temp_loc) 
+  writeLines(all_nnf, f)
+  close(f)
+  
+  nested_nets <- RCy3::importNetworkFromFile(temp_loc) ##First SUID returned if for first network (in this case one with pathways)
+  unlink(temp_loc) #Delete file
+  
+  return(nested_nets)
+}
+
