6916445cf4df34c7106338ef9626eb8a3fcc2745
Mike Hellstern <mike.hellstern2@gmail.com>
2020-03-24 21:00:56 -0700

    Added quick NetGSA function

---
+++ R/NetGSAq.R
@@ -0,0 +1,24 @@
+NetGSAq <-
+  function(
+    x, 	
+    group,    
+    pathways, 	
+    lambda_c = 1,
+    file_e = NULL,
+    file_ne = NULL,
+    lklMethod="REHE",
+    cluster = TRUE,
+    sampling = FALSE,
+    sample_n = NULL,
+    sample_p = NULL, 
+    sample_seeds = NULL,
+    minsize=5,
+    eta=0.1,           
+    lim4kappa=500
+  ){
+    dbs <- unique(as.character(graphite::pathwayDatabases()$database))
+    adj_mats <- prepareAdjMat(x, group, databases = dbs, cluster = cluster, file_e = file_e, file_ne = file_ne, lambda_c = lambda_c, eta = eta)
+    net_est  <- NetGSA(adj_mats$Adj, x, group, pathways, lklMethod = "REHE", sampling = sampling, sample_n = sample_n,
+                       sample_p = sample_p, sample_seeds = sample_seeds, minsize = minsize, eta = eta, lim4kappa = lim4kappa)
+    return(net_est)
+}
